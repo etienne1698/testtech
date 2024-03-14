c84b13cdfd4e58cd38d87c8f3ff5c4c74183d27f
etienne1698 <etienne.robert1698@gmail.com>
2024-03-14 12:01:53 +0100

    navigate is OK

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { UserService } from '../user.service';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'login',
   standalone: false,
@@ -8,14 +9,18 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private router: Router) {}
 
   error = signal<string | undefined>(undefined);
 
   form = new FormGroup({
     email: new FormControl('', {
       nonNullable: true,
-      validators: [Validators.required, Validators.minLength(6), Validators.email],
+      validators: [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.email,
+      ],
     }),
     password: new FormControl('', {
       nonNullable: true,
@@ -25,13 +30,18 @@ export class LoginComponent {
 
   login() {
     if (!this.form.valid) return;
+
     // @ts-ignore
-    this.userService.login({ ...this.form.value }).subscribe({
+    const sub = this.userService.login({ ...this.form.value }).subscribe({
       error: (err) => {
         console.error(err);
         this.error.set(err);
+        sub.unsubscribe();
       },
-      next(value) {},
+      next: (value) => {
+        sub.unsubscribe();
+        this.router.navigateByUrl('/');
+      },
     });
   }
 }
