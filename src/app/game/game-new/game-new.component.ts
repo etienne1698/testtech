3ef4252c0c1cf1effa00e2a02c593af4fd87b603
etienne1698 <etienne.robert1698@gmail.com>
2024-03-14 14:37:52 +0100

    init game

--- src/app/game/game-new/game-new.component.ts
+++ src/app/game/game-new/game-new.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'game-new',
@@ -7,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './game-new.component.css'
 })
 export class GameNewComponent {
+  grid = signal<Array<Array<[string]>>>([])
 
+  form = new FormGroup({
+    rows: new FormControl(6),
+    cols: new FormControl(6),
+  })
 }
--- src/app/game/game.module.ts
+++ src/app/game/game.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GameNewComponent } from './game-new/game-new.component';
 import { GameCellComponent } from './game-cell/game-cell.component';
+import { SharedModule } from '../shared/shared.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [GameNewComponent, GameCellComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
+    ReactiveFormsModule
   ],
   exports: [GameNewComponent, GameCellComponent]
 })
