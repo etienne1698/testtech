56724a4ccba45d4558d72f4ac667d438e0c84f8d
etienne1698 <etienne.robert1698@gmail.com>
2024-03-14 13:06:35 +0100

    directives (need to check why extends problem)

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { FormFieldComponent } from './form-field/form-field.component';
 import { LoadingComponent } from './loading/loading.component';
 import { UserConnectedDirective } from './user-connected.directive';
+import { UserDisconnectedDirective } from './user-disconnected.directive';
 
 @NgModule({
-  declarations: [FormFieldComponent, LoadingComponent, UserConnectedDirective],
+  declarations: [FormFieldComponent, LoadingComponent, UserConnectedDirective, UserDisconnectedDirective],
   imports: [CommonModule],
-  exports: [FormFieldComponent, LoadingComponent, UserConnectedDirective],
+  exports: [FormFieldComponent, LoadingComponent, UserConnectedDirective, UserDisconnectedDirective],
 })
 export class SharedModule {}
--- src/app/shared/user-disconnected.directive.ts
+++ src/app/shared/user-disconnected.directive.ts
@@ -1,15 +1,34 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, OnDestroy, OnInit } from '@angular/core';
 import { UserService } from '../user/user.service';
-
+import { User } from '../user/models/user';
+import { Subscription } from 'rxjs';
 
 @Directive({
   selector: '[appUserDisconnected]',
-  standalone: true,
+  standalone: false,
 })
-export class UserDisconnectedDirective {
-  constructor(private el: ElementRef, private userService: UserService) {
-    if (this.userService.user !== null) {
-      this.el.nativeElement.classList.add('d-none');
+export class UserDisconnectedDirective implements OnInit, OnDestroy {
+  sub?: Subscription;
+
+  protected checkUser(user: User | null, el: Element): void {
+    if (user != null) {
+      el.classList.add('d-none');
+    } else {
+      el.classList.remove('d-none');
     }
+  }
+
+  constructor(private el: ElementRef, private userService: UserService) {
+    this.checkUser(this.userService.user, this.el.nativeElement);
+  }
+
+  ngOnInit(): void {
+    this.sub = this.userService.user$.subscribe((user) =>
+      this.checkUser(user, this.el.nativeElement)
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.sub?.unsubscribe?.();
   }
 }
